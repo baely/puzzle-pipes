427e39d2ac3f726b7d0e7e6366f5e6c6dd8f34f7
baely <5674656+baely@users.noreply.github.com>
2021-09-20 14:56:22 +1000

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ SHAPES = [0, 1, 1, 2, 1, 3, 2, 4, 1, 2, 3, 4, 2, 4, 4]
 
 BOX_DRAWING = [" ", "╺", "╹", "┗", "╸", "━", "┛", "┻", "╻", "┏", "┃", "┣", "┓", "┳", "┫", "╋"]
 
+task = []
 types = []
 rotations = []
 locked = []
 current = []
+param = {}
 
 
 def count_bits(x: int) -> int:
@@ -130,19 +132,19 @@ def has_locked_neighbour(x: int) -> int:
     return max([get_cell(locked, y) for y in get_neighbours(x)])
 
 
-def get_neighbours(x: int) -> set[int]:
+def get_neighbours(x: int) -> list[int]:
     """
     Return the neighbouring positions to position x
     :param x:
     :return:
     """
     p = pos_to_coord(x)
-    return {
+    return [
         coord_to_pos((p[0], p[1] + 1)),
         coord_to_pos((p[0] - 1, p[1])),
         coord_to_pos((p[0], p[1] - 1)),
         coord_to_pos((p[0] + 1, p[1]))
-    }
+    ]
 
 
 def neighbours_locked(*c: int) -> int:
@@ -151,10 +153,7 @@ def neighbours_locked(*c: int) -> int:
     :param c:
     :return:
     """
-    return get_cell(locked, c[0]) + \
-           2 * get_cell(locked, c[1]) + \
-           4 * get_cell(locked, c[2]) + \
-           8 * get_cell(locked, c[3])
+    return get_cell(locked, c[0]) + 2 * get_cell(locked, c[1]) + 4 * get_cell(locked, c[2]) + 8 * get_cell(locked, c[3])
 
 
 def neighbours_facing(*c: int) -> int:
@@ -256,6 +255,7 @@ def contains_loops(x: int) -> (bool, set[int]):
 
 
 def retrieve_new_game() -> str:
+    global param
     # Pull the page
     req = requests.get(f"https://www.puzzle-pipes.com/?size={p_size}")
 
@@ -271,152 +271,161 @@ def retrieve_new_game() -> str:
     return req.text[16650:16650 + SIZE ** 2]
 
 
-def create_new_game(task: str = None) -> None:
+def create_new_game(task_str: str = None) -> None:
+    global task
     global types
     global rotations
     global locked
     global current
-    if task is None:
-        task = retrieve_new_game()
+    if task_str is None:
+        task_str = retrieve_new_game()
 
-    task = [int(c, 16) for c in task]
+    task = [int(c, 16) for c in task_str]
     types = [SHAPES[n] for n in task]
     rotations = [0 for _ in task]
     locked = [0 for _ in task]
     current = task.copy()
 
 
-# Solve the game
-li = True
+def solve() -> None:
+    # Solve the game
+    li = True
 
-while li:
-    # Algorithm 1: Simple algorithm
-    # For all cells, checked if the locked neighbours can determine current rotation
+    while li:
+        # Algorithm 1: Simple algorithm
+        # For all cells, checked if the locked neighbours can determine current rotation
 
-    li = False
+        li = False
 
-    for i in range(SIZE ** 2):
-        if not get_cell(locked, i) and has_locked_neighbour(i):
-            neighbours = get_neighbours(i)
-            locked_neighbours = neighbours_locked(*neighbours)
-            facing_neighbours = neighbours_facing(*neighbours)
+        for i in range(SIZE ** 2):
+            if not get_cell(locked, i) and has_locked_neighbour(i):
+                neighbours = get_neighbours(i)
+                locked_neighbours = neighbours_locked(*neighbours)
+                facing_neighbours = neighbours_facing(*neighbours)
 
-            locked_facing = locked_neighbours & facing_neighbours
-            locked_not_facing = locked_neighbours & ~facing_neighbours
+                locked_facing = locked_neighbours & facing_neighbours
+                locked_not_facing = locked_neighbours & ~facing_neighbours
 
-            if locked_neighbours:
-                rotate_rule(i, locked_neighbours, locked_facing, locked_not_facing)
+                if locked_neighbours:
+                    rotate_rule(i, locked_neighbours, locked_facing, locked_not_facing)
 
-                # Node
-                if types[i] == 1:
-                    if locked_facing or count_bits(locked_not_facing) == 3:
-                        lock(i)
-                        li = True
+                    # Node
+                    if types[i] == 1:
+                        if locked_facing or count_bits(locked_not_facing) == 3:
+                            lock(i)
+                            li = True
 
-                # Bend
-                if types[i] == 2:
-                    if count_bits(locked_neighbours) > 2 or locked_neighbours in (3, 6, 9, 12):
-                        lock(i)
-                        li = True
+                    # Bend
+                    if types[i] == 2:
+                        if count_bits(locked_neighbours) > 2 or locked_neighbours in (3, 6, 9, 12):
+                            lock(i)
+                            li = True
 
-                # Bar
-                if types[i] == 3:
-                    if locked_facing or locked_not_facing:
-                        lock(i)
-                        li = True
+                    # Bar
+                    if types[i] == 3:
+                        if locked_facing or locked_not_facing:
+                            lock(i)
+                            li = True
 
-                # T
-                if types[i] == 4:
-                    if locked_not_facing or count_bits(locked_facing) == 3:
-                        lock(i)
-                        li = True
+                    # T
+                    if types[i] == 4:
+                        if locked_not_facing or count_bits(locked_facing) == 3:
+                            lock(i)
+                            li = True
 
-    if locked_game():
-        break
-else:
-    # Algorithm 2: Complex
-    print("Attempting complex algorithm")
-    print(f"{sum(locked)}/{SIZE**2} locked tiles so far.")
+        if locked_game():
+            break
+    else:
+        pass
+        # # Algorithm 2: Complex
+        # print("Attempting complex algorithm")
+        # print(f"{sum(locked)}/{SIZE ** 2} locked tiles so far.")
+        #
+        # li = False
+        #
+        # for i in range(SIZE ** 2):
+        #     if not get_cell(locked, i) and has_locked_neighbour(i):
+        #         neighbours = get_neighbours(i)
+        #         locked_neighbours = neighbours_locked(*neighbours)
+        #         facing_neighbours = neighbours_facing(*neighbours)
+        #
+        #         locked_facing = locked_neighbours & facing_neighbours
+        #         locked_not_facing = locked_neighbours & ~facing_neighbours
+        #
+        #         # Node
+        #
+        #         # Bend:
+        #         # - If any 1 side is locked. Opposite side must inverse
+        #
+        #         # Bar
+        #
+        #         # T
 
-    li = False
 
-    for i in range(SIZE ** 2):
-        if not get_cell(locked, i) and has_locked_neighbour(i):
-            neighbours = get_neighbours(i)
-            locked_neighbours = neighbours_locked(*neighbours)
-            facing_neighbours = neighbours_facing(*neighbours)
+def submit() -> None:
+    # r = "".join([hex(c)[2:] for c in current])
 
-            locked_facing = locked_neighbours & facing_neighbours
-            locked_not_facing = locked_neighbours & ~facing_neighbours
-
-            # Node
-
-            # Bend:
-            # - If any 1 side is locked. Opposite side must inverse
-
-            # Bar
-
-            # T
-
-
-r = "".join([hex(c)[2:] for c in current])
-# Submit
-
-ans = ""
-for i in range(SIZE):
-    for j in range(SIZE):
-        if 5 == task[coord_to_pos((i, j))] or 10 == task[coord_to_pos((i, j))]:
-            if 2 == rotations[coord_to_pos((i, j))]:
-                ans += "0"
-            elif 3 == rotations[coord_to_pos((i, j))]:
-                ans += "1"
+    ans = ""
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if 5 == task[coord_to_pos((i, j))] or 10 == task[coord_to_pos((i, j))]:
+                if 2 == rotations[coord_to_pos((i, j))]:
+                    ans += "0"
+                elif 3 == rotations[coord_to_pos((i, j))]:
+                    ans += "1"
+                else:
+                    ans += str(rotations[coord_to_pos((i, j))])
             else:
                 ans += str(rotations[coord_to_pos((i, j))])
-        else:
-            ans += str(rotations[coord_to_pos((i, j))])
-ans += ":"
-ans += "".join("0" for _ in range(SIZE ** 2))
+    ans += ":"
+    ans += "".join("0" for _ in range(SIZE ** 2))
 
-obj = {
-    "jstimer": "0", 
-    "jsPersonalTimer": "",
-    "jstimerPersonal": "0",
-    "stopClock": "0", 
-    "fromSolved": "0", 
-    "robot": "1",
-    "zoomslider": "0", 
-    "jstimerShow": "00:00",
-    "jstimerShowPersonal": "00:00",
-    "b": "1", 
-    "size": p_size,
-    "param": param, 
-    "w": SIZE,
-    "h": SIZE,
-    "ansH": ans,
-    "ready": "   Done   "
-}
-
-headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-}
-
-req = requests.post("https://www.puzzle-pipes.com/", obj, headers=headers)
-
-if "Congratulations" in req.text:
-    i = req.text.index("Congratulations")
-    print_box(task)
-    print_box()
-    print(f"{sum(rotations)} moves.", req.text[i:i + 56])
-
-    soup = BeautifulSoup(req.text, features="html.parser")
-    form = soup.find("form", {"action": "/hallsubmit.php"})
-    solparams = form.find("input", {"name": "solparams"}).get("value")
-    data = {
-        "submitscore": 1,
-        "solparams": solparams,
-        "robot": 1,
-        "email": "bailey_fun@live.com.au"
+    obj = {
+        "jstimer": "0",
+        "jsPersonalTimer": "",
+        "jstimerPersonal": "0",
+        "stopClock": "0",
+        "fromSolved": "0",
+        "robot": "1",
+        "zoomslider": "0",
+        "jstimerShow": "00:00",
+        "jstimerShowPersonal": "00:00",
+        "b": "1",
+        "size": p_size,
+        "param": param,
+        "w": SIZE,
+        "h": SIZE,
+        "ansH": ans,
+        "ready": "   Done   "
     }
-    requests.post("https://www.puzzle-pipes.com/hallsubmit.php", data=data, headers=headers)
-elif "Not there yet" not in req.text:
-    print(req.text)
+
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    req = requests.post("https://www.puzzle-pipes.com/", obj, headers=headers)
+
+    if "Congratulations" in req.text:
+        i = req.text.index("Congratulations")
+        print_box(task)
+        print_box()
+        print(f"{sum(rotations)} moves.", req.text[i:i + 56])
+
+        soup = BeautifulSoup(req.text, features="html.parser")
+        form = soup.find("form", {"action": "/hallsubmit.php"})
+        solparams = form.find("input", {"name": "solparams"}).get("value")
+        data = {
+            "submitscore": 1,
+            "solparams": solparams,
+            "robot": 1,
+            "email": "bailey_fun@live.com.au"
+        }
+        requests.post("https://www.puzzle-pipes.com/hallsubmit.php", data=data, headers=headers)
+    elif "Not there yet" not in req.text:
+        print(req.text)
+
+
+if __name__ == '__main__':
+    create_new_game()
+    solve()
+    submit()
